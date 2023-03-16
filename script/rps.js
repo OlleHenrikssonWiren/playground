f46621f919e1cceeb196ce800bd3f90012736bb5
olle-henriksson-wiren-nti-johanneberg <olle.henrikssonwiren@elev.ga.ntig.se>
2023-03-16 19:49:01 +0100

    effects rps

--- script/rps.js
+++ script/rps.js
@@ -1,10 +1,18 @@
 
 
-document.onclick = function testEnabled() { // This is for debugging
-    console.log(keyBtn)
+//document.onclick = function testEnabled() { // This is for debugging
+ //   console.log(keyBtn)
+//}
+let enabled1 = true; // Toggle key regs for player 1
+let enabled2 = true; // Toggle Key regs for player 2 
+var selectionAudio1 = new Audio("../audio/rps/player1Select.ogg");
+var selectionAudio2 = new Audio("../audio/rps/player2Select.wav");
+
+document.querySelector(".resetBtn").onclick = function reset() {
+    enabled1 = true;
+    enabled2 = true;
 }
-    let enabled1 = true; // Toggle key regs for player 1
-    let enabled2 = true; // Toggle Key regs for player 2 
+
 
 //Player "1" key regs.
 document.addEventListener("keyup", (keyPress) = function player1Press(keyPress)  {
@@ -16,18 +24,24 @@ document.addEventListener("keyup", (keyPress) = function player1Press(keyPress) 
         case true:
             console.log(keyBtn);
             SelectDone()
+            playSelectAni() 
+            selectionAudio1.play(); //play audio on select
         break;
 
         case "w": // Code for W
         case true:
             console.log(keyBtn) 
             SelectDone()
+            playSelectAni() 
+            selectionAudio1.play();
         break;
             
         case "e": // Code for E
         case true:
             console.log(keyBtn)
             SelectDone()
+            playSelectAni() 
+            selectionAudio1.play(); 
         break;
 
         }
@@ -54,6 +68,31 @@ document.addEventListener("keyup", (keyPress) = function player1Press(keyPress) 
         enabled1 = false;
         }
     }
+    function playSelectAni() {
+        let player1items = document.getElementsByClassName("player1Item");
+        let player1Backdrop = document.querySelector(".backdrop1");
+        let glowBox1 = document.querySelectorAll(".glowBox1");
+
+        for (let a = 0; a < glowBox1.length; a++) { //För att lägga till en glow effect
+            glowBox1[a].classList.add("glow");
+            setTimeout(function() {
+            glowBox1[a].classList.remove("glow");
+            }, 500)
+        }
+
+
+        for (let i = 0; i < player1items.length; i++) {
+            console.log("animation trigger")
+            player1items[i].classList.remove("selectAni");
+            void player1items[i].offsetWidth; //retrigger
+            player1items[i].classList.add("selectAni");
+        }
+        player1Backdrop.classList.add("backdropPlay");
+        setTimeout(function() {
+        player1Backdrop.classList.remove("backdropPlay");
+        }, 400)
+    }
+
 });
 
 
@@ -67,18 +106,23 @@ document.addEventListener("keyup", (keyPress2) =>  {
         case "i": //Code for left arrow
             console.log(keyBtn2 + "y");
             SelectDone();
+            playSelectAni();
+            selectionAudio2.play(); 
             
         break;
 
         case "o":
             console.log(keyBtn2);
             SelectDone();
-
+            playSelectAni();
+            selectionAudio2.play(); 
         break;
         
         case "p":
             console.log(keyBtn2);
             SelectDone();
+            playSelectAni();
+            selectionAudio2.play(); 
         break;
         }
     } else  {
@@ -103,7 +147,36 @@ document.addEventListener("keyup", (keyPress2) =>  {
         enabled2 = false;
         }
     }
+
+    function playSelectAni() {
+        let player2items = document.getElementsByClassName("player2Item");
+        let player2Backdrop = document.querySelector(".backdrop2");
+        let glowBox2 = document.querySelectorAll(".glowBox2");
+
+
+        for (let a = 0; a < glowBox2.length; a++) { //För att lägga till en glow effect
+            glowBox2[a].classList.add("glow");
+            setTimeout(function() {
+            glowBox2[a].classList.remove("glow");
+            }, 500)
+        }
+            
+
+        for (let i = 0; i < player2items.length; i++) { //För animation shake och backdrop
+            console.log("animation trigger")
+            
+
+            player2items[i].classList.remove("selectAni");
+            void player2items[i].offsetWidth; //retrigger
+            player2items[i].classList.add("selectAni");
+        }
+        player2Backdrop.classList.add("backdropPlay2");
+        setTimeout(function() {
+        player2Backdrop.classList.remove("backdropPlay2");
+        }, 400)
+    }
 });
 
-// What to do after press
+// What to do after press.
+
 
