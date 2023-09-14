7df9e05ac6c37fd74ebdc397f6c47e0599c63400
Olle Henriksson <olle.henrikssonwiren@gmail.com>
2023-09-15 01:58:56 +0200

    added a playing field

--- script/script.js
+++ script/script.js
@@ -95,8 +95,7 @@ window.addEventListener('resize', resize);
 
     var navItemBox = document.getElementById("navItem1st")
     
-	
-	navItemBox.onmouseover = function waveTextHoverOn() { //When you hover over
+navItemBox.onmouseover = function waveTextHoverOn() { //When you hover over
     var navItemText = document.getElementsByClassName("navItemLetter")
     for (var navItemText of navItemText) { // Simple loop to iterate through classes
         navItemText.style.animationName = "navJump"
@@ -219,7 +218,6 @@ ballBox.onmouseout = (event) => {
 	ball.style.border = "0"
 	ball.style.boxShadow = "0"
 	ball.style.opacity = "0"
-	
 	ball.style.transition = "all 0.3s ease-out";
 
 }
@@ -263,169 +261,4 @@ dragBox.onmouseout = (event) => {
 	dragBall.style.top = "0%";
 	dragBall.style.left = "0%";
 	dragBall.style.transform = "translate( 0%, 0%)";
-}
-
-
-
-
-// Minesweep / minröj
-let parts = {
-	amount_rows: 16,
-	amount_cols: 16,
-	amount_bombs: 40,
-	bomb: "💣",
-	alive: true,
-	colors : {
-		1: 'blue', 2: 'green', 3: 'red', 4: 'purple', 5: 'maroon', 6: 'turquoise', 7: 'black', 8: 'grey'
-	}
-}
-
-window.addEventListener('load', function() {
-    document.getElementById('lost').style.display="none";
-    startGame();
-});
-
-function cellID(i, j) {
-    return 'cell-' + i + '-' + j;
-}
-
-function createTable() {
-    var table, row, td, i, j;
-    table = document.createElement('table');
-    
-    for (i=0; i<components.num_of_rows; i++) {
-        row = document.createElement('tr');
-        for (j=0; j<components.num_of_cols; j++) {
-            td = document.createElement('td');
-            td.id = cellID(i, j);
-            row.appendChild(td);
-            addCellListeners(td, i, j);
-        }
-        table.appendChild(row);
-    }
-    return table;
-}
-
-function placeBombs() {
-    var i, rows = [];
-    
-    for (i=0; i<components.amount_bombs; i++) {
-        placeOneBomb(rows);
-    }
-    return rows;
-} 
-
-function placeOneBomb(bombs) {
-
-    var nrow, ncol, row, col;
-    nrow = Math.floor(Math.random() * components.num_of_rows);
-    ncol = Math.floor(Math.random() * components.num_of_cols);
-    row = bombs[nrow];
-    
-    if (!row) {
-        row = [];
-        bombs[nrow] = row;
-    }
-    
-    col = row[ncol];
-    
-    if (!col) {
-        row[ncol] = true;
-        return
-    } 
-    else {
-        placeOneBomb(bombs);
-    }
-}
-
-function adjacentBombs(row, col) {
-    var i, j, num_of_bombs;
-    num_of_bombs = 0;
-
-    for (i=-1; i<2; i++) {
-        for (j=-1; j<2; j++) {
-            if (components.bombs[row + i] && components.bombs[row + i][col + j]) {
-                num_of_bombs++;
-            }
-        }
-    }
-    return num_of_bombs;
-}
-
-function adjacentFlags(row, col) {
-    var i, j, num_flags;
-    num_flags = 0;
-
-    for (i=-1; i<2; i++) {
-        for (j=-1; j<2; j++) {
-            cell = document.getElementById(cellID(row + i, col + j));
-            if (!!cell && cell.flagged) {
-                num_flags++;
-            }
-        }
-	}
-}
-
-
-function handleCellClick(cell, i, j) {
-    if (!components.alive) {
-        return;
-    }
-
-    if (cell.flagged) {
-        return;
-    }
-
-    cell.clicked = true;
-
-    if (components.bombs[i][j]) {
-        cell.style.color = 'red';
-        cell.textContent = components.bomb;
-        gameOver();
-        
-    }
-    else {
-        cell.style.backgroundColor = 'lightGrey';
-        num_of_bombs = adjacentBombs(i, j);
-        if (num_of_bombs) {
-            cell.style.color = components.colors[num_of_bombs];
-            cell.textContent = num_of_bombs;
-        } 
-        else {
-            clickAdjacentBombs(i, j);
-        }
-    }
-}
-
-function clickAdjacentBombs(row, col) {
-    var i, j, cell;
-    
-    for (i=-1; i<2; i++) {
-        for (j=-1; j<2; j++) {
-            if (i === 0 && j === 0) {
-                continue;
-            }
-            cell = document.getElementById(cellID(row + i, col + j));
-            if (!!cell && !cell.clicked && !cell.flagged) {
-                handleCellClick(cell, row + i, col + j);
-            }
-        }
-    }
-}
-
-function performMassClick(cell, row, col) {
-    if (adjacentFlags(row, col) === adjacentBombs(row, col)) {
-        clickAdjacentBombs(row, col);
-    }
-}
-
-
-function gameOver() {
-    components.alive = false;
-    document.getElementById('lost').style.display="block";
-    
-}
-
-function reload(){
-    window.location.reload();
 }
